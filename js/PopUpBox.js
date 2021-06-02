f27ca20f207a5532cafa6b9aff44449fa91d3b68
170776 <59916286+170776@users.noreply.github.com>
2021-06-02 17:00:57 +0300

    Updating Mission and Product Page

---
+++ js/PopUpBox.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    alert("Student house pack"  +
+    "\nA starter pack to help you and your other students out with 1 roll of plastic bags, 2 rolls of green bags, a calendar with when trash is collected in your neighbourhood, 3 Packs of flower seeds and a cool Blossom bag for trips into nature. ");
+  }
+
+  function myFunction1() {
+    alert("Neighbourhood pack"+"\nA advanced pack to help you and your neighbourhood to become cleaner and a better place to live with flyers to inform neighbours, Bio degradable sticker pack, 5 rolls of plastic bags, 10 rolls of green bags. 20 packs of flower seeds, a trash calendar and a nice bag for nice trips trough your cleaner and greener neighbourhood.");
+  }
+  function myFunction2() {
+    alert("Class pack \nDo you want to teach your students about their carbon food print and give them a start into a cleaner and greener future than please reach out to us on our email blossom@hotmail.com for potential guest lectures with of course fun goody bags. ");
+  }
